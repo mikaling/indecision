aed81a5bc66f54f655638680d42a37317bcd123c
Akim Kingori <akim.king12@gmail.com>
2020-05-29 03:30:04 +0300

    Add notification on timer finish

--- app/src/main/java/com/mikaling/indecision/BroadcastService.java
+++ app/src/main/java/com/mikaling/indecision/BroadcastService.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class BroadcastService extends Service {
     private final static String TAG = "BroadcastService";
@@ -38,9 +39,28 @@ public class BroadcastService extends Service {
             public void onFinish() {
                 Log.i(TAG, "Timer finished");
 
+
+                // Create notification
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_access_alarm_black_24dp)
+                        .setContentTitle("Time's up!")
+                        .setContentText("textContent")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                Notification notification = builder.build();
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+                // Remove ongoing notification
+                notificationManager.cancel(12);
+                // Show notification when timer completes
+                // (notificationId is a unique int for each notification that you must define)
+                notificationManager.notify(13, builder.build());
+
+                stopForeground(true);
             }
         }.start();
 
+        // Create notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_access_alarm_black_24dp)
                 .setContentTitle("Get busy!")
@@ -48,6 +68,7 @@ public class BroadcastService extends Service {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         Notification notification = builder.build();
 
+        // Start foreground service with ID of 12 and a specified notification as the ongoing notification
         startForeground(12, notification);
     }
 
